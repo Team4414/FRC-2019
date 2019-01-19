ee4000baf5a1a7031c4a0203da581be353cf2e06
AvidhBavkar <avidhbavkar@gmail.com>
2019-01-18 19:37:02 -0800

    Basic Vision Demo

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,6 +1,10 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import frc.robot.subsystems.Drivetrain;
+import frc.util.Limelight;
+import frc.util.Limelight.CAM_MODE;
+import frc.util.Limelight.LED_STATE;
 
 public class Robot extends TimedRobot {
 
@@ -16,12 +20,17 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-
+    Limelight.setCamMode(CAM_MODE.VISION);
+    Limelight.setLED(LED_STATE.ON);
   }
 
+  double[] visionOffset;
   @Override
   public void autonomousPeriodic() {
-
+    Drivetrain.getInstance().setRawSpeed(
+      -Limelight.tX() * 0.05,
+      Limelight.tX() * 0.05
+    );
   }
 
   @Override
@@ -31,5 +40,10 @@ public class Robot extends TimedRobot {
   @Override
   public void testPeriodic() {
     
+  }
+
+  @Override
+  public void disabledInit() {
+    Limelight.setLED(LED_STATE.OFF);
   }
 }
