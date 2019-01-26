413c388a7e1798875065eaf083c39180e2065d7d
AvidhBavkar <avidhbavkar@gmail.com>
2019-01-26 15:59:26 -0800

    Resolved some warnings in Interpolater

--- src/main/java/frc/util/Interpolater.java
+++ src/main/java/frc/util/Interpolater.java
@@ -1,12 +1,13 @@
 package frc.util;
 
+import java.util.AbstractMap;
 import java.util.LinkedHashMap;
 import java.util.AbstractMap.SimpleEntry;
 
 public class Interpolater{
     public static double get(double c, LinkedHashMap<Double, Double> data){
-        SimpleEntry<Double,Double> lowBound  = new SimpleEntry(Double.MIN_VALUE, 0);
-        SimpleEntry<Double,Double> highBound = new SimpleEntry(Double.MAX_VALUE, 0);
+        SimpleEntry<Double,Double> lowBound  = new SimpleEntry<Double,Double>(Double.MIN_VALUE, 0d);
+        SimpleEntry<Double,Double> highBound = new SimpleEntry<Double,Double>(Double.MAX_VALUE, 0d);
 
         double distLow = Double.MAX_VALUE;
         double distHigh = Double.MAX_VALUE;
@@ -14,12 +15,12 @@ public class Interpolater{
         for (double val : data.keySet()){
             
             if (((c - val) > 0) & ((c - val) < distLow)){
-                lowBound = new SimpleEntry(val, data.get(val));
+                lowBound = new SimpleEntry<Double,Double>(val, data.get(val));
                 distLow = (c - val);
             }
 
             if (((val - c) > 0) & ((val - c) < distHigh)){
-                highBound = new SimpleEntry(val, data.get(val));
+                highBound = new SimpleEntry<Double,Double>(val, data.get(val));
                 distHigh = (c - val);
             }
         }
