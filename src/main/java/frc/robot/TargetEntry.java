4841888958cc4fc020fd71dc92b05f4a5288bc78
AvidhBavkar <avidhbavkar@gmail.com>
2019-01-30 19:34:21 -0800

    Updated Interpolation + moved into TargetEntry
    Updated interpolation to support multiple variables to be interpolated
    (Support TargetEntry). Interpolater utility was too simple and not
    necessary with the addition of another class (TargetEntry) so
    interpolation functionality was moved into TargetEntry as static utility
    methods.

--- src/main/java/frc/robot/TargetEntry.java
+++ src/main/java/frc/robot/TargetEntry.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TargetEntry{
@@ -52,5 +53,49 @@ public class TargetEntry{
     @Override
     public String toString(){
         return (mDistance + "," + mTheta + "," + mHeight+ ",\n");
+    }
+
+
+    public static TargetEntry interpolate(ArrayList<TargetEntry> data, double dist){
+        TargetEntry lowBound = data.get(0);
+        TargetEntry highBound = data.get(0);
+
+        double distLow = Double.MAX_VALUE;
+        double distHigh = Double.MAX_VALUE;
+        double err;
+
+        for (TargetEntry curr: data){
+
+            err = dist - curr.dist();
+
+            if ((err > 0) & (err < distLow)){
+                //if the error is positive (sample is less than target)
+                lowBound = curr;
+                distLow = err;
+            }
+
+            if ((err < 0) & (err < distHigh)){
+                //if the error us negative (sample is above the target)
+                highBound = curr;
+                distHigh = err;
+            }
+        }
+
+
+        return interpolate(highBound, lowBound, dist);
+    }
+
+    private static TargetEntry interpolate(TargetEntry high, TargetEntry low, double dist){
+        //HighDist - LowDist
+        //------------------ * (HighValue + LowValue)
+        //  dist - LowDist
+
+        double pos = ((high.dist() - low.dist()) / (dist - low.dist()));
+
+        return new TargetEntry(
+            dist,
+            pos * (high.theta() + low.theta()),
+            pos * (high.height() + low.height())
+        );
     }
 }
